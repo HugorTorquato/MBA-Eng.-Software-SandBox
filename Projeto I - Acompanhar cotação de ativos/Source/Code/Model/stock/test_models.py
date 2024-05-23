c7759195a4ce956ebadfb14650ef80bbf863ebe8
Hugo Torquato <hugortorquato@gmail.com>
2024-05-22 21:12:00 -0300

    Created a simple unittest

---
+++ Projeto I - Acompanhar cotação de ativos/Source/Code/Model/stock/test_models.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+#from stock.models import Stock
+
+class StockClassTest(TestCase):
+    def test_stock_class_definition(self):
+        #my_stock = Stock("BBAS3", 27.30)
+        #self.assertEqual(my_stock.ticker, "BBAS3", "hug")
+        self.assertEqual(1,1)
