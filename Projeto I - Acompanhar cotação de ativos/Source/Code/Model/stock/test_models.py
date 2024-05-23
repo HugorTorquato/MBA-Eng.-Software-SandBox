9db6b69f90681edc108b0b87bb8a32e18c3f20e1
Hugo Torquato <hugortorquato@gmail.com>
2024-05-22 21:19:24 -0300

    improved test a bit

--- Projeto I - Acompanhar cotação de ativos/Source/Code/Model/stock/test_models.py
+++ Projeto I - Acompanhar cotação de ativos/Source/Code/Model/stock/test_models.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
-#from stock.models import Stock
+from stock.models import Stock, Reits
 
 class StockClassTest(TestCase):
+
     def test_stock_class_definition(self):
-        #my_stock = Stock("BBAS3", 27.30)
-        #self.assertEqual(my_stock.ticker, "BBAS3", "hug")
-        self.assertEqual(1,1)
+        ticker_test = "BBAS3"
+        price_test = 27.30
+        my_stock = Stock(ticker=ticker_test, price=price_test)
+        self.assertEqual(my_stock.ticker, ticker_test)
+        self.assertEqual(my_stock.price, price_test)
+
+    def test_reits_class_definition(self):
+        ticker_test = "IRDM11"
+        price_test = 77.30
+        my_reits = Reits(ticker=ticker_test, price=price_test)
+        self.assertEqual(my_reits.ticker, ticker_test)
+        self.assertEqual(my_reits.price, price_test)
