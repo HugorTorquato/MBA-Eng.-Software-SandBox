f3ec2750abe1dbe3c0474a622ddffcef0a810b15
Hugo Torquato <hugortorquato@gmail.com>
2024-06-08 10:57:56 -0300

    Added the POST method

--- Projeto II - Curso de DJANGO/Source/escola/cursos/views.py
+++ Projeto II - Curso de DJANGO/Source/escola/cursos/views.py
@@ -15,6 +15,19 @@ class CursoAPIView(APIView):
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        # Nesse caso os dados bem do request com a propriedade data
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        #return Response({"msg":"Criado com sucesso"}) # A resposta tbm pode ser uma mensagem
+        #return Response({"id":serializer.data['id']}) # A resposta tbm pode ser uma mensagem com dados
+
+        
+
+
 
 class AvaliacaoAPIView(APIView):
 
@@ -22,3 +35,13 @@ class AvaliacaoAPIView(APIView):
         avaliacores = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacores, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        # Nesse caso os dados bem do request com a propriedade data
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+
+
