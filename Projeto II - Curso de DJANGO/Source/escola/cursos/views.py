e0a4e01024010d907b0ab41112e1ae338701c638
Hugo Torquato <hugortorquato@gmail.com>
2024-06-08 14:56:46 -0300

    Created Routes and Views

--- Projeto II - Curso de DJANGO/Source/escola/cursos/views.py
+++ Projeto II - Curso de DJANGO/Source/escola/cursos/views.py
@@ -1,47 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
+# Duas classes para cada objeto
+# Não pode deixar tudo junto pq temos endpoints de individuo (objetos unicos) e os de coleção
+# que trabalham com conuntos. No caso os que estão no pluração são para verbos HTTP de coleção.
+# E os singulares são os individuais (RETRIAVE, UPDATE, DESTROI)
 
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        # Nesse caso os dados bem do request com a propriedade data
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        #return Response({"msg":"Criado com sucesso"}) # A resposta tbm pode ser uma mensagem
-        #return Response({"id":serializer.data['id']}) # A resposta tbm pode ser uma mensagem com dados
+# CRUD
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    #Endpoins individuais
+    queryset = Curso.objects.all()
+    serialize_class = CursoSerializer
 
-        
+class CursosAPIView(generics.ListCreateAPIView):
+    #Endpoins coletivos
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
+# CRUD
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
 
-
-class AvaliacaoAPIView(APIView):
-
-    def get(self, request):
-        avaliacores = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacores, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        # Nesse caso os dados bem do request com a propriedade data
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
-
-
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
