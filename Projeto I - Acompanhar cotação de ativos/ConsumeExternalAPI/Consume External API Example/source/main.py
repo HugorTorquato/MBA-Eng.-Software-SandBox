ba727c80005f3d3aa987b6eaebcfd56b5894734c
Hugo Torquato <hugortorquato@gmail.com>
2024-07-24 18:34:30 -0300

    Able to consume an API from BRAPI, to get stock info. Free plan

--- Projeto I - Acompanhar cotação de ativos/ConsumeExternalAPI/Consume External API Example/source/main.py
+++ Projeto I - Acompanhar cotação de ativos/ConsumeExternalAPI/Consume External API Example/source/main.py
@@ -1,5 +1,6 @@
 import requests
-
+import os
+from dotenv import load_dotenv
 
 response = requests.get("https://randomuser.me/api/")
 #print(response.text)
@@ -14,8 +15,27 @@ response.text
 
 
 response = requests.get("https://api.thecatapi.com/v1/breeds")
-print(response.status_code) # 200 -> able to comunicate
-print(response.headers)
+#print(response.status_code) # 200 -> able to comunicate
+#print(response.headers)
 request = response.request
-print(request.url)
+#print(request.url)
+
+# Example on how to protect an API key
+load_dotenv()
+api_key = os.getenv("API_KEY")
+print(f'API Key: {str(api_key)}')
+
+url = "https://brapi.dev/api/quote/PETR4"
+params = {
+    'token': {api_key},
+}
+ 
+response = requests.get(url, params=params)
+ 
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Request failed with status code {response.status_code}")
+    print(f"{response.json()}")
 
